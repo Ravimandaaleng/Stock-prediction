e4fe61525854509925d432656bb8956c6ec527ae
Ravimandaaleng <ravimandaal220805@gmail.com>
2026-02-08 15:21:23 +0530

    Homepage

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+     <p className='text-light text-center'>© 2024 - Built with ❤️ by Ravi Kumar.</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button'
+const Main = () => {
+  return (
+    <>
+    <div className='container bg-light-dark rounded'>
+        <div className='p-5 text-center'>
+            <h1 className='text-light'>Welcome to Stock Prediction Portal</h1>
+            <p className='text-light'>Predict stock prices with our advanced AI models.This model is similar to the ESN model in the sense that it can predict stock prices with high accuracy. The model works by capturing historical trend patterns to predict future values with RNN for learning long-term dependencies. Therefore, the LSTM model is used for processing and predicting time-series data</p>
+            <Button text="Get Started" class="btn-outline-info" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
